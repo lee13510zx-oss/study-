322e33f5a8595b61029caade76a999c6c907e9b0
lee13510zx-oss <lee13510zx@gmail.com>
2026-05-19 14:26:03 +0900

    Add React components and .gitignore configuration

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🎓 카테고리 학습 웹사이트</h1>
+      <p>환영합니다!</p>
+    </div>
+  )
+}
+
+export default App
